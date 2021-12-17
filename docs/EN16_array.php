9104fedb3b01c51c331b7895f4a12a74b9ba88df
Frédéric Arijs <frederic.arijs@ext.bewan.be>
2021-12-17 15:15:55 +0100

    Adding a doc directory for documentation and sample PHP array to build the XML

---
+++ docs/EN16_array.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This is a full template of all the fields available in the
+ * AN16 standard
+ *
+ * Value is set to his mandatory state
+ *
+ */
+
+$en16_array = array(
+    'invoiceId' => 'mandatory string',
+    'issueDate' => 'mandatory date string with YYYYMMDD format',
+);
