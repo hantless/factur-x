c82fef3af5d670dbc3971f59a39d6ccc6b72dbea
Frédéric Arijs <hantless@gmail.com>
2021-12-21 00:51:25 +0100

    Adding a skeleton for the array needed to build a EN16931 XML in the documentation

--- docs/EN16931_array.php
+++ docs/EN16931_array.php
@@ -9,13 +9,67 @@
  */
 
 $en16931_array = array(
-    'invoiceId' => 'mandatory string',
+    'invoiceId' => '', // mandatory
     'invoiceType' => 380, // check Types in FacturxXML
-    'issueDate' => 'mandatory date string with YYYYMMDD format',
+    'issueDate' => 'YYYYMMDD', // mandatory
 
-    'lines' => array(
+    'contentNote' => '', // optional
+    'subjectCode' => '', // optional
+
+    'lines' => array( // at least one line mandatory
         array(
+            'product_id' => '', // mandatory
+            'product_name' => '', // mandatory
+            'product_price' => '9.5', // mandatory
+            'billed_quantity' => '', // mandatory
+            'unit_code' => 'C62', // mandatory
+            'tva_category_code' => 'S', // mandatory
+            /**
+            Les codes de catégorie de
+            TVA sont les suivants :
+            • S = Taux de TVA standard
+            • Z = Taux de TVA égal à 0 (non applicable en France)
+            • E = Exempté de TVA
+            • AE = Autoliquidation de TVA
+            • K = Autoliquidation pour cause de livraison intracommunautaire
+            • G = Exempté de TVA pour Export hors UE
+            • O = Hors du périmètre d'application de la TVA
+            • L = Iles Canaries
+            • M = Ceuta et Mellila
+             */
+            'tva_rate' => 20, // 20% => 20
+            'line_total' => 19.9, // excluded VAT, included all charges and other taxes
+
         ),
     ),
+
+    'buyerReference' => '', // optional
+
+    'sellerName' => '', // mandatory
+    'sellerLegalId' => '', // mandatory SIREN/SIRET
+    'sellerLegalIdType' => '', // optional 0002 = SIREN/SIRET
+    'sellerTradingBusinessName' => '', // optional
+    'sellerPostalTradeAddress' => array( // mandatory
+        'countryId' => 'FR', // mandatory
+        'postcode' => '',
+        'lineOne' => '',
+        'cityName' => '',
+        'countrySubDivisionName' => '', // optional
+    ),
+    'sellerTaxRegistrationId' => '', // mandatory
+
+    'buyerName' => '', // mandatory
+    'buyerLegalId' => '',
+    'buyerLegalIdType' => '',
+    'buyerPostalTradeAddress' => array( // mandatory
+        'countryId' => 'FR', // mandatory
+        'postcode' => '',
+        'lineOne' => '',
+        'cityName' => '',
+        'countrySubDivisionName' => '', // optional
+    ),
+    'buyerTaxRegistrationId' => '',
+
+    'invoiceCurrencyCode' => 'EUR', // mandatory
 
 );
